5ab675c97b43c2d1ca91d42fe47e4b0c103a8df7
Dmitry Yavorsky <Yavorskyd@yandex.ru>
2022-10-07 13:51:54 +0300

    Initial Commit

---
+++ HomeApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeApp1
+//
+//  Created by Yavorsky on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
